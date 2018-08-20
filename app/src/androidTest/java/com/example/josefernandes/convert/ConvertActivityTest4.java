6cc9d82c43143dfc210abd90a3398be543073bc9
josefsjunior <josefsjunior@outlook.com>
2018-08-20 00:29:50 -0300

    criacao de teste

--- app/src/androidTest/java/com/example/josefernandes/convert/ConvertActivityTest4.java
+++ app/src/androidTest/java/com/example/josefernandes/convert/ConvertActivityTest4.java
@@ -1,109 +1,37 @@
 package com.example.josefernandes.convert;
 
 
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 
-@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class ConvertActivityTest4 {
 
     @Rule
-    public ActivityTestRule<ConvertActivity_> mActivityTestRule = new ActivityTestRule<>(ConvertActivity_.class);
+    public ActivityTestRule<ConvertActivity_> mActivityTestRule = new ActivityTestRule<>(ConvertActivity_.class, false, true);
 
     @Test
-    public void convertActivityTest4() {
-        ViewInteraction editText = onView(
-                allOf(withId(R.id.convert_value_gasolina),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        editText.perform(click());
-
-        ViewInteraction editText2 = onView(
-                allOf(withId(R.id.convert_value_gasolina),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        editText2.perform(replaceText("3.599"), closeSoftKeyboard());
-
-        ViewInteraction editText3 = onView(
-                allOf(withId(R.id.convert_value_alcool),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                3),
-                        isDisplayed()));
-        editText3.perform(replaceText("-2.699"), closeSoftKeyboard());
-
-        ViewInteraction button = onView(
-                allOf(withId(R.id.convert_button), withText("OK"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                4),
-                        isDisplayed()));
-        button.perform(click());
-
-        ViewInteraction viewGroup = onView(
-                allOf(childAtPosition(
-                        allOf(withId(android.R.id.content),
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-                                        0)),
-                        0),
-                        isDisplayed()));
-        viewGroup.check(matches(isDisplayed()));
-
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+    public void clicarNoBotaoComGasolinaNegativa_apareceMensagemDeErro(){
+        onView(withId(R.id.convert_value_gasolina)).perform(typeText(".999"));
+        closeSoftKeyboard();
+        onView(withId(R.id.convert_value_alcool)).perform(typeText("-2.699"));
+        closeSoftKeyboard();
+        onView(withId(R.id.convert_button)).perform(click());
+        //onView(withText(R.string.abasteca_gasolina)).check(matches(not(isDisplayed())));
+        //onView(withText(R.string.abasteca_alcool)).check(matches(not(isDisplayed())));
     }
 }
