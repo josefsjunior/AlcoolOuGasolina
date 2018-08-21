a66d24bf8f966a2e082fa4d588b548c7c1a2e7f1
josefsjunior <josefsjunior@outlook.com>
2018-08-20 22:36:24 -0300

    nova funcionalidade onde aparece a porcentagem de gasolina em relação ao álcool

--- app/src/main/java/com/example/josefernandes/convert/CalculadorUtil.java
+++ app/src/main/java/com/example/josefernandes/convert/CalculadorUtil.java
@@ -1,17 +1,11 @@
 package com.example.josefernandes.convert;
 
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import static com.example.josefernandes.convert.ConvertActivityConstantes.ALCOOL;
 import static com.example.josefernandes.convert.ConvertActivityConstantes.GASOLINA;
 
 public class CalculadorUtil {
-
-
 
     public static void realizaCalculo(float precoGasolina, float precoAlcool, TextView textoConversao, TextView textoPorcentagem) {
         float porcentagemFloat = precoAlcool / precoGasolina;
