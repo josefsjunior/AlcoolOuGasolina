f3117439afef484399bea52a2872b97403cb9ebb
josefsjunior <josefsjunior@outlook.com>
2018-09-07 23:43:37 -0300

    versao 1.8.1 - insercao formulario de carro

--- app/src/main/java/com/josefernandes/convert/activities/FormularioCarroActivity.java
+++ app/src/main/java/com/josefernandes/convert/activities/FormularioCarroActivity.java
@@ -9,28 +9,29 @@ import android.widget.Toast;
 
 import com.josefernandes.convert.R;
 
-public class CarroActivity extends AppCompatActivity {
+public class FormularioCarroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_carro);
+        setContentView(R.layout.activity_formulario_carro);
 
-        setTitle("Carros");
+        setTitle("Novo Carro");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_carros, menu);
+        getMenuInflater().inflate(R.menu.menu_formulario_carro, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.menu_carro_novo:
-                Toast.makeText(this, "Clicou para adiconar um novo carro", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(this, FormularioCarroActivity.class));
+            case R.id.menu_formulario_carro_ok:
+                Toast.makeText(this, "Clicou no botao pronto", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, CarroActivity.class));
+                finish();
                 break;
         }
         return super.onOptionsItemSelected(item);
